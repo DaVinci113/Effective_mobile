800d78c62801d08218a7261bd1294103176eec85
proger <maxkgd@mail.ru>
2025-04-08 14:30:28 +0200

    add: user ads list

--- Effect_mob/ads/urls.py
+++ Effect_mob/ads/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import AdsList, CreateAd
+from .views import AdsList, CreateAd, UserAdsList
 
 app_name = 'ads'
 urlpatterns = [
     path('', AdsList.as_view(), name='ads-list'),
+    path('create_ad/', CreateAd.as_view(), name='create-ad'),
+    path('ads/user_ads/', UserAdsList.as_view(), name='user-ads'),
     path('create_ad/', CreateAd.as_view(), name='create-ad'),
 ]
--- Effect_mob/ads/views.py
+++ Effect_mob/ads/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth.models import User
-from django.shortcuts import redirect
-from django.template.context_processors import request
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 from .models import Ad
 from .forms import CreateAdForm
 
@@ -15,6 +12,14 @@ class AdsList(ListView):
     template_name = 'ads/ads_list.html'
 
 
+class UserAdsList(ListView):
+    model = Ad
+    template_name = 'ads/user_ads.html'
+
+    def get_queryset(self):
+        return Ad.objects.filter(user=self.request.user)
+
+
 class CreateAd(CreateView):
     form_class = CreateAdForm
     template_name = 'ads/create_ad.html'
@@ -25,3 +30,12 @@ class CreateAd(CreateView):
         instance.user = self.request.user
         instance.save()
         return super().form_valid(form)
+
+
+class UpdateAd(UpdateView):
+    form_class = CreateAdForm
+    fields = '__all__'
+    template_name = 'ads/update_ad.html'
+    pk_url_kwarg = 'ad_pk'
+    success_url = '/ads/user_ads/'
+
